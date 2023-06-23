7fe28080de1713e618e6ae89173358cdd700f697
Shadrack Opoku <shaddyopoku1@gmail.com>
2023-06-23 18:59:48 +0000

    chore: debugin user creation.

--- lib/aft/controller.ts
+++ lib/aft/controller.ts
@@ -5,40 +5,47 @@ import axios from "axios";
 async function HandleUssd(req: Request, res: Response): Promise<Response> {
 	const { sessionId, serviceCode, phoneNumber, text } = req.body;
 	req.headers["content-type"] = "text/plain";
-	//console.log({ sessionId, serviceCode, phoneNumber, text });
+	console.log({ serviceCode, phoneNumber, text });
 	// check if the user already exists.
-	const theUser = await UserModel.findOne({ phoneNumber });
+	try {
+		const theUser = await UserModel.findOne({ phoneNumber });
 
-	// when the user is has already created account.
-	if (theUser) {
-		// if this is the first message
-		if (theUser.firstName && theUser.lastName && theUser.password) {
-			return res.status(200).send("You already have and account.");
-		}
+		// when the user is has already created account.
+		if (theUser) {
+			console.log("user found");
+			// if this is the first message
+			if (theUser.firstName && theUser.lastName && theUser.password) {
+				return res.status(200).send("You already have and account.");
+			}
 
-		if (!theUser.firstName) {
-			theUser.firstName = text;
-			theUser.save();
-			return res.status(200).send("Enter your last name");
-		} else if (!theUser.lastName) {
-			theUser.lastName = text;
-			theUser.save();
-			return res.status(200).send("Enter your password.");
-		} else {
-			theUser.password = text;
-			theUser.save();
-			return res
-				.status(200)
-				.send("You have sucessfully created an account with us.\n");
+			if (!theUser.firstName) {
+				theUser.firstName = text;
+				theUser.save();
+				return res.status(200).send("Enter your last name");
+			} else if (!theUser.lastName) {
+				theUser.lastName = text;
+				theUser.save();
+				return res.status(200).send("Enter your password.");
+			} else {
+				theUser.password = text;
+				theUser.save();
+				return res
+					.status(200)
+					.send("You have sucessfully created an account with us.\n");
+			}
 		}
+	} catch (e) {
+		console.log("failed to find a user.", { e });
+		res.status(200).send("an unknown error occurred while trying to find the user.");
 	}
-
 	// when there is no account.
 	//
+	console.log("there was no user.")
 	switch (text) {
 		case "1":
 			//const fd = new FormData();
 			//register the user.
+			console.log("got to 1")
 			try {
 				await UserModel.create({ phoneNumber });
 				return res.status(200).send("CON  please enter your first name.\n");
@@ -50,8 +57,10 @@ async function HandleUssd(req: Request, res: Response): Promise<Response> {
 			}
 
 		case "2":
+			console.log("got to 2")
 			return res.status(200).send("Ok comeback next time.");
 		default:
+			console.log("got to the default");
 			return res.status(200).send("Incorrect input please try again.");
 		//register the user using sms
 	}
